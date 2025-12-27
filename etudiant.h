6b899c7ca35781233a7cf6fd5a9ac85378a5da29
ewidaltayou <ewidalt@gmail.com>
2025-12-27 06:00:03 +0100

    mise en formrs du fichier d'entete

--- etudiant.h
+++ etudiant.h
@@ -1,9 +1,10 @@
 #ifndef ETUDIANT_H
 #define ETUDIANT_H
+
 // structure de la date
 typedef struct{
     int jour;
-    int moi;
+    int mois;
     int annee;
 }Date;
 // structure de l'etudiant
@@ -11,35 +12,36 @@ typedef struct{
    char matricule[20];
    char nom[20];
    char prenom[20];
-   Date datenaissance;
+   Date dateNaissance;
    char departement[15];
    char filiere[30];
    char region[10];
    char sexe;
-   char filiere[15];
 }Etudiant;
 // gestion de la date
-Date* creerDate();
 int dateValide(Date*d);
+int anneeCourante();
+Date creerDate();
+int verifierDate(Date d);
 // operations de CRUD des etudiants
-Etudiant* creerEtudiant(const char*nomFichier);
+Etudiant creerEtudiant(const char*nomFichier);
 int modifierEtudiant(const char*matricule,const char*nomFichier);
 int supprimerEtudiant(const char*matricule,const char*nomFichier);
-void afficherEtudint(Etudiant*e);
+void afficherEtudint(Etudiant e);
 // operations supplementaires
-int calculAge(const char*matricule);
+int calculAge(const char*matricule,const char*nomFichier);
 int nombreEtudiant(const char*nomFichier);
-int verifierMatriclule(const char*matricule);
+int verifierMatricule(const char*matricule);
 void afficherTousLesEtudiant(const char*nomFichier);
+void genererMatricule( char matricule[], int annee, int numero);
+int prochainNumero();
 //pour les differentes recherches
 int rechercherEtudiantParMatricule(const char*matricule,const char*nomFichier);
 int rechercherEtudiantPardichotomique(const char*matricule,const char*nomFichier);
 int rechercherEtudiantParNom(const char*matricule,const char*nomFichier);
 // pour les differents tri
-void tierParNom(const char*nomFichier);
+void trierParNom(const char*nomFichier);
 void trierParMatricule(const char*nomFichier);
 void trierParRegion(const char*nomFichier);
-#endif
-
-
-
+void trierParFiliere(const char*nomFichier);
+#endif 
